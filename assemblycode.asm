08af063aa44412e04993b599db804ae03b3fc2e2
peasent3 <143019464+peasent3@users.noreply.github.com>
2025-05-05 13:34:39 -0400

    Update assemblycode.asm

--- assemblycode.asm
+++ assemblycode.asm
@@ -1,21 +1,7 @@
-;
-; AssemblerApplication3.asm
-<<<<<<< HEAD
-=======
-;
-; Created: 1/21/2025 11:26:45 AM
-; Author : 1933124
-;
-; Replace with your application code
 
+; Author : Kinjie Supe
 ;
-; Lab10.asm
->>>>>>> 9da6e9a0b7477aacbbbc52c50b9393cf9050b925
 ;
-; Created: 1/21/2025 11:26:45 AM
-; Author : 1933124
-;
-; Replace with your application code
 .EQU BUSON = $80
 .EQU VAL = $96 
 .EQU ADR = $3000 ; adress specified
